d20be478d8553dedb6256bc47ca5b78c48cfe8a0
GoodLvck <x17albaserrano17x@gmail.com>
2021-02-01 11:33:43 +0100

    tarea 61 js

--- JavaScript/Practicas/2. Bucles/Tarea61/functions.js
+++ JavaScript/Practicas/2. Bucles/Tarea61/functions.js
@@ -1,1 +1,10 @@
-var numeros = [2, 3, 5, 10]
+var numeros = [0, 10]
+var suma = 0
+
+for (var i in numeros) {
+    suma += numeros[i]
+}
+
+var media = suma / numeros.length
+
+document.write("<br>" + "La media es: " + media)
