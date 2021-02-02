969b4dd2ba0a2c121c3b8619144ccfcf34aa2bda
GoodLvck <x17albaserrano17x@gmail.com>
2021-02-02 10:59:24 +0100

    examen

--- JavaScript/Practicas/2. Bucles/Tarea63/functions.js
+++ JavaScript/Practicas/2. Bucles/Tarea63/functions.js
@@ -1,7 +1,8 @@
-var numeros = [1, 5, 7, 10, 11]
+var numeros = [1, 5, 7, 7, 10, 11]
+numeros.sort
 
 if (numeros.length % 2 == 0){
-    var mediana =  (numeros[numeros.length - 1] + numeros[numeros.length]) / 2
+    var mediana =  (numeros[numeros.length / 2 - 1] + numeros[numeros.length / 2])
 } else {
     var mediana = muneros[(numeros.length - 1)/2]
 }
